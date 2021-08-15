b9f1696fece3d635848264800079e9d401f3cdfa
Kristin Collins <kristin@tighten.co>
2021-08-15 13:40:21 -0400

    Add default social tags to layout.

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,9 +1,24 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
+    <title>When Pigs Fly</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Laravel</title>
+    <meta name="description" content="A blog dedicated to the love of pigs.">
+
+    <meta property="og:title" content="When Pigs Fly">
+    <meta property="og:description" content="A blog dedicated to the love of pigs.">
+    <meta property="og:url" content="http://www.whenpigsfly.com">
+    <meta property="og:type" content="website">
+    <meta property="og:image" content="{{ asset('img/flying-pig.jpeg') }}">
+    <meta property="og:image:width" content="500" />
+    <meta property="og:image:height" content="344" />
+
+    <meta name="twitter:title" content="When Pigs Fly">
+    <meta name="twitter:description" content="A blog dedicated to the love of pigs.">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:image" content="{{ asset('img/flying-pig.jpeg') }}">
+
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
